853ca0965c956406d0cf694b8bde70aeb043cf1a
jvfe <jvfecav@gmail.com>
2022-10-06 14:30:08 -0300

    refactor: Change binning to map_tasks

--- wf/binning.py
+++ wf/binning.py
@@ -1,26 +1,54 @@
 import subprocess
+from dataclasses import dataclass
 from pathlib import Path
+from random import sample
+from typing import List
 
-from latch import large_task, message, small_task, workflow
+from dataclasses_json import dataclass_json
+from latch import large_task, map_task, message, small_task, workflow
 from latch.types import LatchDir, LatchFile
 
+from .assembly import MegaHitOut
 from .types import Sample
 
 
+@dataclass
+@dataclass_json
+class BwAlignInput:
+    assembly_index: LatchDir
+    read_data: Sample
+
+
+@dataclass
+@dataclass_json
+class JgiInput:
+    assembly_bam: LatchFile
+    sample_name: str
+
+
+@dataclass
+@dataclass_json
+class MetaBatInput:
+    sample_name: str
+    assembly_data: LatchDir
+    depth_file: LatchFile
+
+
 @large_task
-def bowtie_assembly_build(assembly_dir: LatchDir, sample: Sample) -> LatchDir:
+def bowtie_assembly_build(megahit_out: MegaHitOut) -> LatchDir:
 
-    assembly_name = f"{sample.sample_name}.contigs.fa"
-    assembly_fasta = Path(assembly_dir.local_path, assembly_name)
+    sample_name = megahit_out.sample_name
+    assembly_name = f"{sample_name}.contigs.fa"
+    assembly_fasta = Path(megahit_out.assembly_data.local_path, assembly_name)
 
-    output_dir_name = f"{sample.sample_name}_assembly_idx"
+    output_dir_name = f"{sample_name}_assembly_idx"
     output_dir = Path(output_dir_name).resolve()
     output_dir.mkdir(parents=True, exist_ok=True)
 
     _bt_idx_cmd = [
         "bowtie2/bowtie2-build",
         str(assembly_fasta),
-        f"{str(output_dir)}/{sample.sample_name}",
+        f"{str(output_dir)}/{sample_name}",
         "--threads",
         "31",
     ]
@@ -28,28 +56,40 @@ def bowtie_assembly_build(assembly_dir: LatchDir, sample: Sample) -> LatchDir:
     subprocess.run(_bt_idx_cmd)
 
     return LatchDir(
-        str(output_dir), f"latch:///metamage/{sample.sample_name}/{output_dir_name}"
+        str(output_dir), f"latch:///metamage/{sample_name}/{output_dir_name}"
     )
 
 
-@large_task
-def bowtie_assembly_align(
-    assembly_idx: LatchDir,
-    sample: Sample,
-) -> LatchFile:
+@small_task
+def organize_bwalign_inputs(
+    assembly_idxs: List[LatchDir], samples: List[Sample]
+) -> List[BwAlignInput]:
 
-    output_file_name = f"{sample.sample_name}_assembly_sorted.bam"
+    inputs = []
+    for sample, assembly_idx in zip(samples, assembly_idxs):
+        cur_input = BwAlignInput(assembly_index=assembly_idx, read_data=sample)
+
+        inputs.append(cur_input)
+
+    return inputs
+
+
+@large_task
+def bowtie_assembly_align(bwalign_input: BwAlignInput) -> LatchFile:
+
+    sample_name = bwalign_input.read_data.sample_name
+    output_file_name = f"{sample_name}_assembly_sorted.bam"
 
     output_file = Path(output_file_name).resolve()
 
     _bt_cmd = [
         "bowtie2/bowtie2",
         "-x",
-        f"{assembly_idx.local_path}/{sample.sample_name}",
+        f"{bwalign_input.assembly_idx.local_path}/{sample_name}",
         "-1",
-        sample.read1.local_path,
+        bwalign_input.read_data.read1.local_path,
         "-2",
-        sample.read2.local_path,
+        bwalign_input.read_data.read2.local_path,
         "--threads",
         "31",
     ]
@@ -85,42 +125,61 @@ def bowtie_assembly_align(
         stdin=sam_convert_out.stdout,
     )
 
-    return LatchFile(
-        str(output_file), f"latch:///metamage/{sample.sample_name}/{output_file_name}"
+    return JgiInput(
+        assembly_bam=LatchFile(
+            str(output_file), f"latch:///metamage/{sample_name}/{output_file_name}"
+        ),
+        sample_name=sample_name,
     )
 
 
 @small_task
-def summarize_contig_depths(assembly_bam: LatchFile, sample: Sample) -> LatchFile:
+def summarize_contig_depths(jgi_input: JgiInput) -> LatchFile:
 
-    output_file_name = f"{sample.sample_name}_depths.txt"
+    sample_name = jgi_input.sample_name
+    output_file_name = f"{sample_name}_depths.txt"
     output_file = Path(output_file_name).resolve()
 
     _jgi_cmd = [
         "jgi_summarize_bam_contig_depths",
         "--outputDepth",
         output_file_name,
-        assembly_bam.local_path,
+        jgi_input.assembly_bam.local_path,
     ]
 
     subprocess.run(_jgi_cmd)
 
     return LatchFile(
-        str(output_file), f"latch:///metamage/{sample.sample_name}/{output_file_name}"
+        str(output_file), f"latch:///metamage/{sample_name}/{output_file_name}"
     )
 
 
+@small_task
+def organize_metabat_inputs(
+    assembly_data: List[MegaHitOut],
+    depth_files: List[LatchFile],
+) -> MetaBatInput:
+
+    inputs = []
+    for assembly, depth_file in zip(assembly_data, depth_files):
+        cur_input = MetaBatInput(
+            sample_name=assembly.sample_name,
+            assembly_data=assembly.assembly_data,
+            depth_file=depth_file,
+        )
+        inputs.append(cur_input)
+
+    return inputs
+
+
 @large_task
-def metabat2(
-    assembly_dir: LatchDir,
-    depth_file: LatchFile,
-    sample: Sample,
-) -> LatchDir:
+def metabat2(metabat_input: MetaBatInput) -> LatchDir:
 
-    assembly_name = f"{sample.sample_name}.contigs.fa"
-    assembly_fasta = Path(assembly_dir.local_path, assembly_name)
+    sample_name = metabat_input.sample_name
+    assembly_name = f"{sample_name}.contigs.fa"
+    assembly_fasta = Path(metabat_input.assembly_data.local_path, assembly_name)
 
-    output_dir_name = f"METABAT/{sample.sample_name}"
+    output_dir_name = f"METABAT/{sample_name}"
     output_dir = Path(output_dir_name).parent.resolve()
 
     _metabat_cmd = [
@@ -129,7 +188,7 @@ def metabat2(
         "-i",
         str(assembly_fasta),
         "-a",
-        depth_file.local_path,
+        metabat_input.depth_file.local_path,
         "-o",
         output_dir_name,
     ]
@@ -142,24 +201,28 @@ def metabat2(
     )
     subprocess.run(_metabat_cmd)
 
-    return LatchDir(str(output_dir), f"latch:///metamage/{sample.sample_name}/METABAT/")
+    return LatchDir(str(output_dir), f"latch:///metamage/{sample_name}/METABAT/")
 
 
 @workflow
-def binning_wf(sample: Sample, assembly_dir: LatchDir) -> LatchDir:
+def binning_wf(samples: List[Sample], megahit_out: List[MegaHitOut]) -> List[LatchDir]:
 
     # Binning preparation
-    built_assembly_idx = bowtie_assembly_build(assembly_dir=assembly_dir, sample=sample)
-    aligned_to_assembly = bowtie_assembly_align(
-        assembly_idx=built_assembly_idx, sample=sample
+    built_assembly_idxs = map_task(bowtie_assembly_build)(megahit_out=megahit_out)
+
+    bwalign_inputs = organize_bwalign_inputs(
+        assembly_idxs=built_assembly_idxs, samples=samples
     )
-    depth_file = summarize_contig_depths(
-        assembly_bam=aligned_to_assembly, sample=sample
+
+    aligned_to_assembly = map_task(bowtie_assembly_align)(bwalign_input=bwalign_inputs)
+
+    depth_files = map_task(summarize_contig_depths)(jgi_input=aligned_to_assembly)
+
+    metabat_inputs = organize_metabat_inputs(
+        assembly_data=megahit_out, depth_files=depth_files
     )
 
     # Binning
-    binning_results = metabat2(
-        assembly_dir=assembly_dir, depth_file=depth_file, sample=sample
-    )
+    binning_results = map_task(metabat2)(metabat_input=metabat_inputs)
 
     return binning_results
