bc0ed1d0979981b2f8be3ee214571d5583342c67
jvfe <jvfecav@gmail.com>
2022-12-15 15:46:36 -0300

    docs: Add flow and rules to wf docs

--- wf/docs.py
+++ wf/docs.py
@@ -1,18 +1,19 @@
-from latch.types import LatchAuthor, LatchMetadata, LatchParameter
-
-metamage_DOCS = LatchMetadata(
-    display_name="MetaMage-quick",
-    documentation="https://github.com/jvfe/metamage-quick_latch/blob/main/README.md",
-    author=LatchAuthor(
-        name="jvfe",
-        github="https://github.com/jvfe",
-    ),
-    repository="https://github.com/jvfe/metamage-quick_latch",
-    license="MIT",
-    tags=["NGS", "metagenomics", "MAG"],
+from latch.types.metadata import (
+    Fork,
+    ForkBranch,
+    LatchAuthor,
+    LatchMetadata,
+    LatchParameter,
+    LatchRule,
+    Params,
+    Section,
+    Spoiler,
+    Text,
 )
 
-metamage_DOCS.parameters = {
+dmp_rule = LatchRule(regex=".dmp$", message="Must be a .dmp file")
+
+PARAMS = {
     "samples": LatchParameter(
         display_name="Sample data",
         description="Paired-end FASTQ files",
@@ -21,7 +22,6 @@ metamage_DOCS.parameters = {
     "k_min": LatchParameter(
         display_name="Minimum kmer size",
         description="Must be odd and <=255",
-        section_title="MEGAHIT parameters",
     ),
     "k_max": LatchParameter(
         display_name="Maximum kmer size",
@@ -40,15 +40,20 @@ metamage_DOCS.parameters = {
     "kaiju_ref_db": LatchParameter(
         display_name="Kaiju reference database (FM-index)",
         description="Kaiju reference database '.fmi' file.",
-        section_title="Kaiju parameters",
+        detail="(.fmi)",
+        rules=[LatchRule(regex=".fmi", message="Must be an .fmi index file")],
     ),
     "kaiju_ref_nodes": LatchParameter(
         display_name="Kaiju reference database nodes",
         description="Kaiju reference nodes, 'nodes.dmp' file.",
+        detail="(.dmp)",
+        rules=[dmp_rule],
     ),
     "kaiju_ref_names": LatchParameter(
         display_name="Kaiju reference database names",
         description="Kaiju reference taxon names, 'names.dmp' file.",
+        detail="(.dmp)",
+        rules=[dmp_rule],
     ),
     "taxon_rank": LatchParameter(
         display_name="Taxonomic rank (kaiju2table)",
@@ -57,10 +62,53 @@ metamage_DOCS.parameters = {
     "prodigal_output_format": LatchParameter(
         display_name="Prodigal output file format",
         description="Specify main output file format (one of gbk, gff or sco).",
-        section_title="Functional analysis parameters",
     ),
     "fargene_hmm_model": LatchParameter(
         display_name="fARGene's HMM model",
         description="The Hidden Markov Model that should be used to predict ARGs from the data",
     ),
 }
+
+FLOW = [
+    Section(
+        "Samples",
+        Text(
+            "Sample provided has to include an identifier for the sample (Sample name)"
+            " and two files corresponding to the reads (paired-end)"
+        ),
+        Params("samples"),
+    ),
+    Section(
+        "Assembly parameters",
+        Text("Parameters for the assembly software MEGAHIT"),
+        Params("k_min", "k_max", "k_step", "min_count", "min_contig_len"),
+    ),
+    Section(
+        "Taxonomic classification",
+        Text(
+            "Parameters for the taxonomic classifier Kaiju"
+            " choose which database to use and at which taxonomic"
+            " level the final TSV report should be generated"
+        ),
+        Params("kaiju_ref_db", "kaiju_ref_nodes", "kaiju_ref_names", "taxon_rank"),
+    ),
+    Section(
+        "Functional annotation parameters",
+        Text("Options for the functional annotation subworkflow"),
+        Params("prodigal_output_format", "fargene_hmm_model"),
+    ),
+]
+
+metamage_DOCS = LatchMetadata(
+    display_name="MetaMage-quick",
+    documentation="https://github.com/jvfe/metamage-quick_latch/blob/main/README.md",
+    author=LatchAuthor(
+        name="jvfe",
+        github="https://github.com/jvfe",
+    ),
+    repository="https://github.com/jvfe/metamage-quick_latch",
+    license="MIT",
+    parameters=PARAMS,
+    tags=["NGS", "metagenomics", "MAG"],
+    flow=FLOW,
+)
